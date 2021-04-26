83ecb8fc0808207de06cfb59f60654ad92bca652
SwathiDhanwada-MSFT <48976275+SwathiDhanwada-MSFT@users.noreply.github.com>
2021-04-26 23:31:11 +0530

    Creating DiagnosticSettingsApp.ps1 to update
    Creating DiagnosticSettingsApp.ps1 to update diagnostic settings of App Service

---
+++ PowerShell Scripts/DiagnosticSettings.ps1
@@ -0,0 +1,22 @@
+param([string] $workspaceId)
+param([string] $azDiagName)
+param([string] $ResourceId)
+
+$azResources = Get-AzResource -ResourceType 'Microsoft.Web/Sites' -ResourceId $ResourceId
+
+foreach ($azResource in $azResources) {
+    $resourceId     = $azResource.ResourceId
+    $azDiagSettings = Get-AzDiagnosticSetting -ResourceId $resourceId | Where-Object {$_.Id -ne $NULL}
+
+    if($azDiagSettings -eq $null){
+        Set-AzDiagnosticSetting -ResourceId $resourceId -WorkspaceId $workspaceId -Name $azDiagName -Enabled $true
+    }
+    else{
+        foreach ($azDiag in $azDiagSettings) {
+            If (!($azDiag.WorkspaceId -eq $workspaceId -and $azDiag.Name -eq $azDiagName)) {
+                Remove-AzDiagnosticSetting -ResourceId $resourceId -Name $azDiag.Name
+                Set-AzDiagnosticSetting -ResourceId $resourceId -WorkspaceId $workspaceId -Name {$azDiagName -Enabled $true
+            }         
+        }
+    }
+}
