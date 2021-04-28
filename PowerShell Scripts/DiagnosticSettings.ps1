c9c367e76cf0a63366a99c850119ea02c8309def
SwathiDhanwada-MSFT <48976275+SwathiDhanwada-MSFT@users.noreply.github.com>
2021-04-28 12:36:34 +0530

    Update DiagnosticSettings.ps1

--- PowerShell Scripts/DiagnosticSettings.ps1
+++ PowerShell Scripts/DiagnosticSettings.ps1
@@ -1,6 +1,3 @@
-#[string] [Parameter(Mandatory=$false)] $workspaceId,
-#[string] [Parameter(Mandatory=$false)] $azDiagName
-
 param(
     [string] $workspaceId,
     [string] $azDiagName
@@ -8,16 +5,9 @@ param(
 
 $azResources = Get-AzResource -ResourceType 'Microsoft.Web/sites'
 
-Write-Output $azResources
-
-Write-Output $workspaceId
-Write-Output $azDiagName
-
 foreach ($azResource in $azResources) {
     $resourceId     = $azResource.ResourceId
-    Write-Output $resourceId
     $azDiagSettings = Get-AzDiagnosticSetting -ResourceId $resourceId 
-    Write-Output $azDiagSettings
 
     if([string]::IsNullOrEmpty($azDiagSettings)){
         Set-AzDiagnosticSetting -ResourceId $resourceId -WorkspaceId $workspaceId -Name $azDiagName -Enabled $true
